796b659d26ecef1289a12ba0ec4616899cecb19c
BoringCat <c654477757@gmail.com>
2020-05-01 18:36:39 +0800

    修复无法连接数据库的问题

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,7 +1,10 @@
 from config import envconfig
+from utils import createLogger
 
+_dblog = createLogger('db.__init__')
 __all__ = ['CacheDB']
-_dbconfs = envconfig.gets('DB_HOST','DB_PORT','DB_USER','DB_PASSWD','DB_NAME','DB_AUTHDB')
+_dbconfs = list(envconfig.gets('DB_HOST','DB_PORT','DB_USER','DB_PASSWD','DB_NAME','DB_AUTHDB'))
+_dblog.debug('_dbconfs: %s' % str(dict(zip(['DB_HOST','DB_PORT','DB_USER','DB_PASSWD','DB_NAME','DB_AUTHDB'],_dbconfs))))
 if tuple(filter(lambda x:bool(x), _dbconfs)):
     from .mongo import tempdb
     CacheDB = tempdb(*_dbconfs)
