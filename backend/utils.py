26808117a6533b8c65fcb1bd5bbbb46601eaf820
boringcat <c654477757@gmail.com>
2020-05-06 11:07:18 +0800

    过滤分割图片，增加注释

--- backend/utils.py
+++ backend/utils.py
@@ -12,10 +12,21 @@ _isinit = False
 __all__ = [ 'createLogger', 'setLogLevel', 'GetCVAsync' ]
 
 def createLogger(name):
+    '''创建一个Logger对象。用于格式化日志输出
+
+      args:
+        - name: 根对象命名
+    '''
     if not _isinit: _init_logconfig()
     return logging.getLogger(name)
 
+# 初始化Logger
 def _init_logconfig():
+    '''创建一个Logger对象。用于格式化日志输出
+
+    **你不应该在外部调用它！**
+    '''
+    # 检查日志输出等级，VIEW: 输出到info级、DEBUG: 输出到DEBUG级（覆盖VIEW）
     view = envconfig.get('VIEW', False)
     debug = envconfig.get('DEBUG', False)
     logconfig = {
@@ -25,24 +36,67 @@ def _init_logconfig():
     }
     logging.basicConfig(**logconfig)
     _isinit = True
-    
+
+# 设置日志输出等级，用于取消某些包的DEBUG输出
 def setLogLevel(name, level):
+    '''设置日志输出等级，可以用于取消某些包的DEBUG输出
+
+    **你不应该在外部调用它！**
+
+      args:
+        - name: 对象日志命名
+        - level: logger的等级
+    '''
     logging.getLogger(name).setLevel(level)
 
 def start_loop(loop:asyncio.BaseEventLoop):
+    '启动asyncio循环'
     loop.run_forever()
 
 def readCV(webtext):
+    '''分析图片们
+
+    **你不应该在外部调用它！**
+
+    args:
+      - webtext: 页面HTML文本
+    '''
     bs = BeautifulSoup(webtext, features="html.parser")
-    haveHead = bs.find('meta', {'data-hid': "og:image"})
+    haveHead = bs.find('meta', {'data-hid': "og:image"})    # 获取封面
     head_img = haveHead.attrs.get('content') if haveHead else None
-    ulist = tuple(filter(lambda x:bool(x), map(lambda x:x.attrs.get('data-src'), bs.find('div','article-holder').find_all('img'))))
-    return { 'header': head_img, 'contents': ['@'.join(u.split('@')[:-1]) if '@' in u else u for u in ulist] }
+    ulist = tuple(filter(                               # 过滤获取不到的（一般没有）
+        lambda x:bool(x),
+        map(                                            # 获取data-src
+            lambda x:x.attrs.get('data-src'),
+            filter(                                     # 过滤分割图片（`cut-off-\d+`）
+                lambda x:'cut-off' not in ('\n'.join(x.attrs.get('class',[]))),
+                bs.find('div','article-holder').find_all('img')
+            )
+        )
+    ))
+    return { 
+        'header': head_img,
+        'contents': ['@'.join(u.split('@')[:-1]) if '@' in u else u for u in ulist] 
+    }
 
 def getCVid(url):
+    '''获取CV的ID
+
+    **你不应该在外部调用它！**
+
+    args:
+      - url: CV的URL
+    '''
     return int(url.split('/')[-1][2:])
 
 class GetCVAsync():
+    '''异步获取CV图片类
+    kwargs:
+      - fatherlog: logger对象
+    
+    funcs:
+      - Get(url, reqheader, callback, loop)
+    '''
     def __init__(self, fatherlog):
         self._log = fatherlog.getChild('GetCVAsync')
         self._econf = envconfig
@@ -64,6 +118,13 @@ class GetCVAsync():
         self._session = session()
 
     def filter_headers(self, headers):
+        '''过滤无用头部避免出错
+
+        **你不应该在外部调用它！**  
+        
+        kwargs:
+          - headers: 头部dict
+        '''
         headers.pop('Host', None)
         headers.pop('Upgrade', None)
         headers.pop('Connection', None)
@@ -73,6 +134,13 @@ class GetCVAsync():
                 headers.pop(k, None)
 
     async def GetQueue(self, interval = 10):
+        '''获取CV图片列表队列
+
+        **你不应该在外部调用它！**  
+        
+        kwargs:
+          - interval: 每次分析的间隔（单位：秒）（默认：10）
+        '''
         while self._GoRUN:
             try:
                 (url, reqheader, callback, loop) = self._getqueue.get()
@@ -81,18 +149,28 @@ class GetCVAsync():
                 res = self._session.get(url, headers = reqheader)
                 if res.status_code == 200:
                     cvid = getCVid(url)
-                    asyncio.run_coroutine_threadsafe(callback(True, cvid, readCV(res.text), loop),loop)
+                    asyncio.run_coroutine_threadsafe(callback(True, cvid, readCV(res.text)),loop)
                 else:
-                    asyncio.run_coroutine_threadsafe(callback(False, None, res, loop),loop)
+                    asyncio.run_coroutine_threadsafe(callback(False, None, res),loop)
             except Empty:
                 pass
             except Exception as err:
                 self._log.getChild('GetQueue').error("Type: %s, msg: %s" % (type(err),str(err)))
-                asyncio.run_coroutine_threadsafe(callback(False, None, None, loop),loop)
+                asyncio.run_coroutine_threadsafe(callback(False, None, None),loop)
             finally:
                 await asyncio.sleep(interval)
 
     def Get(self, url, reqheader, callback, loop):
+        '''异步获取CV图片列表
+        kwargs:
+          - url: CV的URL
+          - reqheader: 用户的请求头
+          - callback: websocket的回调
+          - callback: 回调所在的asyncio loop
+        
+        return:
+          (status:bool, errmsg:str)
+        '''
         try:
             self._getqueue.put((url, reqheader, callback, loop))
         except Full:
--- backend/wsapi.py
+++ backend/wsapi.py
@@ -7,17 +7,33 @@ from utils import getCVid
 from db import CacheDB as db
 
 def disconnect(msg):
+    '''WebSocket格式化断开信息
+
+    args:
+      - msg: 信息
+    '''
     return dumps({
         'action': "disconnect",
         'msg': msg
     },ensure_ascii=False,separators=(',',':'))
 
 def response(**msg):
+    '''WebSocket格式化返回信息
+
+    args:
+      - **msg: 信息键值对
+    '''
     b = {'action': "response"}
     b.update(msg)
     return dumps(b,ensure_ascii=False,separators=(',',':'))
 
 async def readbcv(websocket:websockets.server.WebSocketServerProtocol, path):
+    '''WebSocket处理函数
+
+    kwargs:
+      - websocket: websocket对象
+      - path: 连接的路径
+    '''
     fatherlog = createLogger('readbcv_ws')
     econf = envconfig
     glo_path = econf.get('APP_PATH', '/')
@@ -27,21 +43,27 @@ async def readbcv(websocket:websockets.server.WebSocketServerProtocol, path):
     if path != glo_path:
         await websocket.close(1001, "Path error")
         return
-    async def callback(status, cvid, msg, loop):
-        if status:
-            db.Cache(cvid, msg)
+    async def callback(status, cvid, msg):
+        '''回调函数
+
+        kwargs:
+          - status: 回调过来的状态
+          - cvid: cv的ID
+          - msg: 要返回的信息（包括错误信息）
+        '''
+        if status:                  # 如果分析正常
+            db.Cache(cvid, msg)     # 写入缓存
             await websocket.send(response(status=True, imgs=msg))
-        else:
-            if msg:
+        else:                       # 分析失败
+            if msg:                 # B站返回状态码
                 errmsg = '服务器返回%d' % msg.status_code
                 await websocket.send(response(status = False, errmsg = errmsg))
-            else:
+            else:                   # 其他错误
                 await websocket.send(response(status = False, errmsg = '内部错误'))
-        lock.set()
-        fatherlog.getChild('callback.lock.is_set').debug(lock.is_set())
+        lock.set()                  # 解除阻塞
         fatherlog.getChild('callback').debug('UnLock!')
 
-    try:
+    try:                            # 尝试获取请求内容
         postdict = loads(await websocket.recv())
     except Exception:
         await websocket.send(disconnect('出错'))
@@ -51,18 +73,18 @@ async def readbcv(websocket:websockets.server.WebSocketServerProtocol, path):
     if not url:
         await websocket.send(disconnect('出错'))
         await websocket.close(1001, "error")
-    cache = db.getCache(getCVid(url))
-    if cache:
-        lock.set()
+    cache = db.getCache(getCVid(url))   # 读缓存，判断是否能从缓存返回
+    if cache:                           # 从缓存返回
+        lock.set()                      # 解除阻塞
         await websocket.send(response(status=True, imgs=cache, fromcache=True))
         await websocket.close()
         return
-    status, msg = getcv.Get(url, websocket.request_headers, callback, get_event_loop())
+    status, msg = getcv.Get(url, websocket.request_headers, callback, get_event_loop()) # 提交任务到队列
     fatherlog.debug('Status: %s, msg: %s' % (status, msg))
-    if not status:
+    if not status:                      # 提交失败（一般是被DOS或爬，导致队列已满）（或者代码出错:(）
         await (websocket.close(1001, "服务器限制") if msg == 'Full' else websocket.close(1001, "内部错误"))
         return
     fatherlog.debug('WaitLock!')
-    await lock.wait()
+    await lock.wait()                   # 阻塞，等待回调
     fatherlog.debug('UnLock!')
-    await websocket.close()
+    await websocket.close()             # 信息在回调处发送，这里直接关闭链接
